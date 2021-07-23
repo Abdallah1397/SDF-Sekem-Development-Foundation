c2eed90f4996178f7b5ed2fcfbda14cc9d56718c
Abdallah1397 <abdallahattallah197@gmail.com>
2021-07-23 17:32:54 +0200

    'handel_about_page'

--- src/component/aboutContnet/aboutContnet.js
+++ src/component/aboutContnet/aboutContnet.js
@@ -2,16 +2,44 @@ import React from "react";
 import "./aboutContent.scss";
 
 // Content Componet that take the topic and title to make an awesome topic
-const AboutContent = ({topic,topic1,topic2,topic3, title }) => {
+const AboutContent = ({ display,topic, topic1, topic2, topic3, title, img ,list1,list2,list3,list4,list5,list6,list7,list8,list9,list10,list11,list12,list13}) => {
   return (
-    <div className="aboutContent" data-aos="zoom-in-right" data-aos-duration="2000"  >
-    <h2 className="aboutContent__title">{title}</h2>
-    <p className="aboutContent__paragraph mt-5">{topic}</p>
-    <p className="aboutContent__paragraph mt-5">{topic1}</p>
-    <p className="aboutContent__paragraph mt-5">{topic2}</p>
-    <p className="aboutContent__paragraph mt-5">{topic3}</p>
-
-
+    <div
+      className="aboutContent"
+      data-aos="zoom-in-right"
+      data-aos-duration="2000"
+    >
+      <h2 className="aboutContent__title">{title}</h2>
+      <img className="aboutContent__img" width="250px" height="250px" src={img} />
+      <p className="aboutContent__div">
+        <p className="aboutContent__P">
+          <p className="aboutContent__paragraph mt-5">{topic}</p>
+          <p className="aboutContent__paragraph mt-5">{topic1}</p>
+          <p className="aboutContent__paragraph mt-5">{topic2}</p>
+          <p className="aboutContent__paragraph mt-5">{topic3}</p>
+          <ul
+            className="about-ul"
+            data-aos="zoom-in-right"
+            data-aos-duration="2000"
+            style={{display:display}}
+            
+          >
+            <li style={{listStyle:"none"}}> {list1}</li>
+            <li style={{listStyle:"none"}}>{list2}</li>
+            <li style={{listStyle:"none"}}> {list3}</li>
+            <li style={{listStyle:"none"}}> {list4}</li>
+            <li style={{listStyle:"none"}}> {list5}</li>
+            <li style={{listStyle:"none"}}> {list6}</li>
+            <li style={{listStyle:"none"}}> {list7}</li>
+            <li style={{listStyle:"none"}}> {list8}</li>
+            <li style={{listStyle:"none"}}> {list9}</li>
+            <li style={{listStyle:"none"}}> {list10}</li>
+            <li style={{listStyle:"none"}}> {list11}</li>
+            <li style={{listStyle:"none"}}> {list12}</li>
+            <li style={{listStyle:"none"}}> {list13}</li>
+          </ul>
+        </p>
+      </p>
     </div>
   );
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,25 @@
 import AboutContent from "../component/aboutContnet/aboutContnet";
 import SliderBanner from "../component/SliderBanner/sliderBanner";
 import Title from "../component/Title/Title";
+import sekem from "../assets/images/aboutus/sekem.jpg";
+import vocational from "../assets/images/aboutus/VocationalTrainingCenter2.png";
+import sekemEdu from "../assets/images/aboutus/sekem-4.jpg";
+import sekemEdu1 from "../assets/images/aboutus/sekem5.jpg";
+import sekemSp from "../assets/images/aboutus/sekemSpecialEdu.jpg";
+import sekemEdu2 from "../assets/images/aboutus/sekem6.jpg";
+import sekem2 from "../assets/images/aboutus/FeaturedImage_SESC_campaign.jpg";
+import sekem3 from "../assets/images/aboutus/sekem3.jpg";
+import sekem4 from "../assets/images/aboutus/sekem4.jpg";
+import sekem5 from "../assets/images/aboutus/sekem5.jpg";
+import sekem6 from "../assets/images/aboutus/sekem6.jpg";
+import sekem7 from "../assets/images/aboutus/sekem7.jpg";
+import sekem8 from "../assets/images/aboutus/sekem8.jpg";
+import sekem9 from "../assets/images/aboutus/sekem9.jpg";
+import sekem10 from "../assets/images/aboutus/sekem10.jpg";
+import sekem11 from "../assets/images/aboutus/sekem11.png";
+import sekem12 from "../assets/images/aboutus/sekem12.jpg";
+import sekem13 from "../assets/images/aboutus/sekem13.jpg";
+
 
 const About = () => {
   return (
@@ -11,39 +30,54 @@ const About = () => {
         title="Individual. Inspirational. Cultural."
         topic="In order to contribute to the developmental goals of the Egyptian people and to facilitate the realization of Egypt’s unique contribution to global advancement and progress, SEKEM established the SEKEM Development Foundation (SDF). The Foundation works to improve the quality of people’s lives by creating awareness and raising consciousness, as well as improving living conditions. This emphasis on the connection between improved standards of living and enhanced self-awareness is the core principle and must be accompanied by an enhancement of people’s morale and culture in order to be effective. Over the last two decades, the foundation has expanded its program of activities from initial basic educational initiatives and it now implements a variety of projects and programs in the field of social development, health care, education and ecology.
         Vocational Training Center ."
+        img={sekem}
+        display="none"
       />
       <AboutContent
         title="Learning by Doing – Get Ready for Work"
         topic="The Vocational Training Center (VTC) provides young people with abilities that are needed in the local labor market. Given the current lack of opportunities in the Egyptian labor market, the curriculum prepares students for self-employment. Through training courses, the apprentices are guided through every aspect of their chosen trade. In line with the SEKEM philosophy “learning by doing and doing by learning”, on-the-job training is emphasized and practical skills are judged to be just as important as theory. Trainees participate in a three-year program, taught by both local and foreign-trained staff. Since 1999, the VTC has been accredited by the Initiative for Industrial Schools for Vocational Training (formerly the Mubarak-Kohl-Initiative), a highly successful German technical assistance project in Egypt. Accredited courses include:"
+        img={vocational}
+        list1="Mechanics"
+        list2="Plumbing"
+        list3="Ready Made Garment"
+        list4="Agriculture Machinery Mechanics"
+        list5="General Administration<"
+        list6="Carpentry"
+        list7="Electronic Technology (incl. computer maintenance)"
+        list8="Photovoltaic installation and maintenance"
+        list9="Organic Agriculture"
+
+
       />
-      <ul className="about-ul" data-aos="zoom-in-right" data-aos-duration="2000">
-        <li> Mechanics</li>
-        <li> Plumbing</li>
-        <li> Ready Made Garment</li>
-        <li> Agriculture Machinery Mechanics</li>
-        <li> General Administration</li>
-        <li> Carpentry</li>
-        <li> Electronic Technology (incl. computer maintenance)</li>
-        <li> Photovoltaic installation and maintenance</li>
-        <li> Organic Agriculture</li>
-      </ul>
       <AboutContent
         title="Education is More than Learning"
         topic="Education is the foundation of holistic human development and advancement of all. It is a continuous challenge. Learning throughout life enables people to improve their living conditions and contribute to the development of the community and the country."
         topic1="The SEKEM School was founded in 1989 and includes a primary, preparatory, and secondary school for 300 pupils. It is situated near Bilbeis, about 60 km. northeast of Cairo, on the premises of SEKEM.  The pupils represent all social levels and come mainly from the nearby town of Bilbeis or the surrounding rural neighborhoods. The school serves Muslim and Christian children alike, encouraging them to live in harmony and have respect for the other’s religious practices."
         topic2="Although the SEKEM School is approved by the Egyptian Ministry of Education and is based on the Egyptian State Curriculum, it also promotes new forms of educational and social interaction. Besides focusing on improving pupils’ knowledge and skills, the SEKEM School also emphasizes programs which cultivate the integrity and character of the individual. Thus conventional education is enriched with courses in eurythmy, crafts, drama, dance or music. By combining traditional educational methods with innovative learning experiences, it is possible to foster the maximum social, cultural and educational development of each child. Working and learning are interrelated and are both practiced at all age levels. For this, the SEKEM School provides spacious and well equipped facilities, including workshops and a theatre."
+        img={sekemEdu}
+        display="none"
       />
       <AboutContent
         title="Education for Everyone"
         topic="Students come to SEKEM schools from rural, semi-urban and urban areas; from underprivileged and privileged families; from children of professional and managerial parents; from children of technical and skilled laborers; from children of farmers and unskilled workers; and from native Egyptians and foreigners alike. Students are also diversified with respect to their educational needs. There are those who seek regular public schooling, to those who desire technical education and vocational training, to those who have special educational needs, and even to those who are school dropouts or have had no opportunity for any type of literate education. Needless to say, that this broad diversity of the student body provides educational and learning experiences that cannot be matched or gained in any other real-life situation."
+        img={sekemEdu1}
+        display="none"
+
+
       />
       <AboutContent
         title="The Specific Educational Programs"
         topic="In addition to the General School System with a nursery, Kindergarten, primary, preparatory, and secondary school, a second educational program at SEKEM is the Vocational and Technical Education System. Established in 1997 and accredited by Mubarak-Kohl Technical Program in 1999, the aim of this technical education program is to offer young people useful professional skills that are in demand in the local labor market. The three-year technical program stresses “learning by doing and doing while learning”, as well as on the job training, practices, and preparation."
+        img={sekemSp}
+        display="none"
+
       />
       <AboutContent
         title="A Small Step for Us. A Big for the Babies."
         topic="Many years ago, SEKEM established the SEKEM Kindergarten as a nursery that was also to provide services to young mothers working in SEKEMs factories. Over the years, the small facility has grown and become indispensable to many workers on the original SEKEM farm. In 2012 the much-liked service has received an addition. A new group specifically dedicated to caring for babies and toddlers has been inaugurated. The new offering is again aimed at young mothers working in the factories on the farm. Due to its central and easily accessible location, working mothers can now bring their children with them even if they are still very young. They may be brought in at 8 in the morning right before the regular work day starts. Mothers will then pick them up again by 5pm in the evening when they leave the premises."
+        img={sekemEdu1}
+        display="none"
+
       />
       <AboutContent
         title="Creative and Interactive Children through Holistic Education"
@@ -51,10 +85,16 @@ const About = () => {
         topic1="The SEKEM Kindergarten provides a strong foundation for children to develop and become active participants in life-long learning process. The program is designed for children from four to six years, with special pedagogic emphasis on creative play and discovery to ensure the individual and social development of each young child."
         topic2="As young children live in a rich world of imagination, play and discovery, they are completely open and deeply influenced by all that surrounds them. The sensory experiences play a major role in their development. The little child grasps the world through his fingers and embarks in discovering it. In a loving and secure environment, the children learn about themselves and their world."
         topic3="Their days are filled with practical and artistic work, imaginative play and fairy tales, puppetry and music, circle games and healthy outdoor pursuits. Great emphasis is put on the development of all senses, in particular on the children’s imagination and creativity."
+        img={sekemEdu2}
+        display="none"
+
       />
       <AboutContent
         title="Touching the World – Experience Life Through Interactive Science"
         topic="The SEKEM Environmental Science Center (SESC) offers interactive science classes on environmental topics to students from the SEKEM Community, as well as from local and international schools. The one-day field trips consisting of practical activities cover a wide variety of subjects including chemistry, biology, physics and geography. "
+        img={sekem2}
+        display="none"
+
       />
       <AboutContent
         title="Giving Chances to Everyone – Integration Into the Community"
@@ -62,65 +102,88 @@ const About = () => {
         topic1="Believing that people with special needs have the capability to be effective in the community is the vision behind the establishment of this school. The school provides care, training and rehabilitation for those people as well as integrates them in the society."
         topic2="In this School people with special needs exercise their role in life according to their abilities and through the development of mental/cognitive and physical/mobility skills, the Sekem school enables special-needs pupils to interact with their environment, by using such tools as music, arts and crafts, and academic classes. All of this helps to increase the awareness of special-needs pupils, enables them to develop their individual skills, and find their own place in society."
         topic3="SEKEM takes this one step further by trying to create job opportunities within the institution for those who are less capable, to help them achieve maximum independence and social integration."
+        img={sekem3}
+        display="none"
+
       />
       <AboutContent
         title="SEKEMs Community School the “Chamomile Children"
         topic="Child labor is a widespread social problem in Egypt and other developing countries. Many poor families cannot afford to live without the income children bring into the family and, hence, many children are taken out of school far too early if they join at all."
         topic1="To alleviate the child labor problem and to provide as many children as possible with the most essential skills, the SEKEM Community School was designed to stimulate creativity and thinking as a basis for further vocational training such as carpentry and mechanics. The educational part is coupled with an opportunity to gain income through adequate work, mainly related to the chamomile harvest (which is why those children are called “Chamomile Children”). This program is officially approved by the Egyptian government and covered by the Egyptian law that allows light work for children between 12 and 14, especially in the agricultural sector, if it is coupled with adequate training and the right working conditions."
+        img={sekem6}
+        display="none"
+
       />
       <AboutContent
         title="Giving the Poorest a Chance"
         topic="The children also have the option to enroll at the SEKEM Preparatory School, although this path is rarely chosen due to the need to gain an early income."
         topic1="The curriculum at the Community School in SEKEM encompasses subjects like English, Mathematics, Arabic and Religion. To improve health care and hygiene the SEKEM Medical Center conducts awareness programs and offers medical services free of charge. Warm, nutritious meals and adequate clothing are provided for the children as well. Later on, our Chamomile Children are able to join the Vocational Training Center in order to receive practical training."
+        img={sekem7}
+        display="none"
+
       />
       <AboutContent
         title="Life Will Reveal for Those Whose Perception is Educated Through Art"
         topic="Art is the window through which man can realize his higher faculties. It is not the goal but a way to address people through the lively interaction of all forces that they can experience the unfolding of their individuality, discover the hidden aspects of their humanity and to make it manifest. Art can initiate an individual transformation process, which trains the awareness of pupils and helps them develop an artistic foundation, thus enabling more precise observation of the environment and closer involvement in global development issues."
+        img={sekem8}
+        display="none"
+
       />
       <AboutContent
         title="Inspire Yourself Through Music, Movement, Speech and Arts"
         topic="Another objective of the arts is to continually develop the individual’s skills and perception. The artistic impulse in SEKEM provides every employee with the opportunity to discover the artist in themselves. SEKEM is marked by an artistic impulse even in architecture. Business and culture are tightly interwoven."
         topic1="Through artistic exercises we help to support the economic processes and strengthen the cooperation between science and arts. SEKEM employees are introduced to language and artistic exercise such as music, painting, and eurythmy (art of movement). Through the interaction of drama, eurythmy and music, we would like to roll out SEKEMs artistic impulse to the wider public in Egypt. We offer professional training courses in the arts, which are also open to new ideas resulting from our own research."
+        img={sekem9}
+        display="none"
+
       />
       <AboutContent
         title="Medical Service for Everyone and Everyday"
+        img={sekem10}
         topic="Provision of health care and access to medical services are among the most basic human rights. The SEKEM Medical Center is providing both services to SEKEM employees, students and surrounding 40.000 community members. Curative treatment is provided to the patients through the Center’s different clinics which are managed by specialized physicians. Preventive services are provided by public-health physicians and social workers. They raise the people’s awareness about sanitation and hygiene, provide them with information on health, nutrition, disease prevention and environmental issues."
+        display="none"
+
       />
-      <AboutContent title="The Medical Center Comprises the Following Health Services:" />
-      <ul className="about-ul" data-aos="zoom-in-right" data-aos-duration="2000">
-        <li > General practitioner and Family Physician</li>
-        <li> Pediatrics</li>
-        <li> Internal Medicine and Gastroenterology</li>
-        <li> Cardiology</li>
-        <li> Dermatology</li>
-        <li> Dentistry</li>
-        <li> Ear, Nose and Throat</li>
-        <li> Minor and General Surgery</li>
-        <li> Neurosurgery</li>
-        <li> Urology</li>
-        <li> Orthopedics</li>
-        <li> Ophthalmology</li>
-        <li> Obstetrics and Gynaecology</li>
-      </ul>
-      <AboutContent title="Diagnostic Services:" />
-      <ul className="about-ul" data-aos="zoom-in-right" data-aos-duration="2000">
-        <li> X – rays</li>
-        <li> Sonography</li>
-        <li> Endoscopy</li>
-        <li> Colonoscopy</li>
-        <li> Doppler-rays</li>
-        <li> Clinical Pathology Labaratory and medical analysis</li>
-      </ul>
-      <AboutContent title="Service & Therapy:" />
-      <ul className="about-ul" data-aos="zoom-in-right" data-aos-duration="2000">
-        <li> Pharmacy and Physiotherapy</li>
-      </ul>
+      <AboutContent title="The Medical Center Comprises the Following Health Services:" img={sekem11}
+        list1="General practitioner and Family Physician"
+        list2="Pediatrics"
+        list3="Internal Medicine and Gastroenterology "
+        list4="Cardiology"
+        list5="Dermatology"
+        list6="Ear, Nose and Throat"
+        list7="Minor and General Surgery"
+        list8="Neurosurgery"
+        list9="Urology"
+        list10="Dentistry"
+        list11="Orthopedics"
+        list12="Ophthalmology"
+        list13="Obstetrics and Gynaecology"
+      />
+
+      <AboutContent title="Diagnostic Services"
+        img={sekem12} 
+        list1="X – rays"
+        list2="Sonography"
+        list3="Endoscopy "
+        list4="Colonoscopy"
+        list5="Doppler-rays"
+        list6="Clinical Pathology Labaratory and medical analysis"
+        />
+   
+      {/* <AboutContent title="Service & Therapy"
+        topic="Pharmacy and Physiotherapy"
+        img={sekem13}
+        display="none" /> */}
+
       <AboutContent
         title="Giving Chances to Everyone – Integration Into the Community"
         topic="The objective of this program is to foster, encourage and guide people with special needs to self-reliance as well as to engage them in the on-going business processes as much as possible."
         topic1="Believing that people with special needs have the capability to be effective in the community is the vision behind the establishment of this school. The school provides care, training and rehabilitation for those people as well as integrates them in the society."
         topic2="In this School people with special needs exercise their role in life according to their abilities and through the development of mental/cognitive and physical/mobility skills, the Sekem school enables special-needs pupils to interact with their environment, by using such tools as music, arts and crafts, and academic classes. All of this helps to increase the awareness of special-needs pupils, enables them to develop their individual skills, and find their own place in society."
         topic3="SEKEM takes this one step further by trying to create job opportunities within the institution for those who are less capable, to help them achieve maximum independence and social integration."
+        img={sekem3}
+        display="none"
+
       />
     </div>
   );
